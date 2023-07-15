90f4f12e4aac65f3dae858d5a59ccffe4cdacf51
JoaoFrancisco11 <joaofrancisco.monteiro11@gmail.com>
2023-07-14 22:23:56 -0300

    Criaçaõ do projeto

---
+++ src/main/java/com/client/ws/ramooplus/WsRasmooPlusApplication.java
@@ -0,0 +1,13 @@
+package com.client.ws.ramooplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsRasmooPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsRasmooPlusApplication.class, args);
+	}
+
+}
